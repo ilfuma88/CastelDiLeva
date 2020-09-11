0846d36cc46f4e347a078686e17c3d0cec741332
GabryLele98 <46486689+GabryLele98@users.noreply.github.com>
2020-09-12 01:38:19 +0200

    Finita DealsActivity.java (activity delle offerte) - GabryLele98

--- CasteldiLeva/app/src/main/java/it/ilfuma/rc/casteldileva/DealsActivity.java
+++ CasteldiLeva/app/src/main/java/it/ilfuma/rc/casteldileva/DealsActivity.java
@@ -10,11 +10,13 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
 public class DealsActivity extends AppCompatActivity {
     private Holder holder;
+    private int cntPopupWindow = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class DealsActivity extends AppCompatActivity {
         private Context mContext;
         private CardView cvDiscount, cvComplimentaryServices, cvSpecialPackets;
         private PopupWindow mPopupWindow;
+        private ImageButton ibtnClosePopup;
         private ConstraintLayout clDeals;
 
         public Holder(Context context) {
@@ -48,34 +51,54 @@ public class DealsActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.cvDiscount:
-                    showPopupWindow(R.id.cvDiscount);
+                    if (cntPopupWindow == 0) {
+                        showPopupWindow(R.id.cvDiscount);
+                        cntPopupWindow += 1;
+                    }
                     break;
                 case R.id.cvComplimentaryServices:
+                    if (cntPopupWindow == 0) {
+                        showPopupWindow(R.id.cvComplimentaryServices);
+                        cntPopupWindow += 1;
+                    }
                     break;
                 case R.id.cvSpecialPackets:
+                    if (cntPopupWindow == 0) {
+                        showPopupWindow(R.id.cvSpecialPackets);
+                        cntPopupWindow += 1;
+                    }
                     break;
+
+                case R.id.ibtnClosePopup:
+                    mPopupWindow.dismiss(); // Chiudo la finestra di popup
+                    cntPopupWindow = 0;
             }
         }
 
         public void showPopupWindow(int id) {
-            switch (id) {
-                case R.id.cvDiscount:
-                    LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);  // Inizializzo una nuova istanza del LayoutInflater service
-                    View popupDiscount = inflater.inflate(R.layout.popup_discount, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
-
-                    mPopupWindow = new PopupWindow(popupDiscount, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);   // Inizializzo una nuova istanza di una finestra popup
-
-                    // Imposto un valore di elevation per la finestra di popup (per API >= 21)
-                    if (Build.VERSION.SDK_INT >= 21) {
-                        mPopupWindow.setElevation(5.0f);
-                    }
-
-                    mPopupWindow.showAtLocation(clDeals, Gravity.CENTER, 0, 0);  // Visualizza la finestra di popup al centro del ConstraintLayout del root
-
-                    //ibtnClosePopup = popupDiscount.findViewById(R.id.ibtnClosePopup);
-                    //ibtnClosePopup.setOnClickListener(this);
-                    break;
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);  // Inizializzo una nuova istanza del LayoutInflater service
+            View popup = null;
+            if (id == R.id.cvDiscount) {
+                popup = inflater.inflate(R.layout.popup_discount, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
             }
+            if (id == R.id.cvComplimentaryServices) {
+                popup = inflater.inflate(R.layout.popup_complimentary_services, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
+            }
+            if (id == R.id.cvSpecialPackets) {
+                popup = inflater.inflate(R.layout.popup_special_packets, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
+            }
+
+            mPopupWindow = new PopupWindow(popup, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);   // Inizializzo una nuova istanza di una finestra popup
+
+            // Imposto un valore di elevation per la finestra di popup (per API >= 21)
+            if (Build.VERSION.SDK_INT >= 21) {
+                mPopupWindow.setElevation(5.0f);
+            }
+
+            mPopupWindow.showAtLocation(clDeals, Gravity.CENTER, 0, 0);  // Visualizza la finestra di popup al centro del ConstraintLayout del root
+
+            ibtnClosePopup = popup.findViewById(R.id.ibtnClosePopup);
+            ibtnClosePopup.setOnClickListener(this);
         }
     }
 }
