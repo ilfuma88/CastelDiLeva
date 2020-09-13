bb67791ea87470e5bf0819761f7e8e64f899df9f
GabryLele98 <46486689+GabryLele98@users.noreply.github.com>
2020-09-13 13:44:45 +0200

    DealsActivity.java commentata - GabryLele98

--- CasteldiLeva/app/src/main/java/it/ilfuma/rc/casteldileva/DealsActivity.java
+++ CasteldiLeva/app/src/main/java/it/ilfuma/rc/casteldileva/DealsActivity.java
@@ -15,9 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+/**
+ * La DealsActivity gestisce l'attività relativa alle offerte dei negozi,
+ * mostrando dei popup che permettono di visualizzare i negozi aderenti alle offerte.
+ */
 public class DealsActivity extends AppCompatActivity {
     private Holder holder;
-    private int cntPopupWindow = 0;
+    private int cntPopupWindow = 0; // conta quante finestre di Popup sono aperte. All'inizio nessuna.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +32,10 @@ public class DealsActivity extends AppCompatActivity {
     }
 
     class Holder implements View.OnClickListener {
-        private Context mContext;
+        private Context mContext;   // e' il contesto in cui gira l'Activity
         private CardView cvDiscount, cvComplimentaryServices, cvSpecialPackets;
-        private PopupWindow mPopupWindow;
-        private ImageButton ibtnClosePopup;
+        private PopupWindow mPopupWindow;   // la finestra di Popup che mostrera' le offerte dopo il click delle CardView
+        private ImageButton ibtnClosePopup; // il bottone che permette la chiusura del popup
         private ConstraintLayout clDeals;
 
         public Holder(Context context) {
@@ -43,50 +47,60 @@ public class DealsActivity extends AppCompatActivity {
             cvComplimentaryServices = findViewById(R.id.cvComplimentaryServices);
             cvSpecialPackets = findViewById(R.id.cvSpecialPackets);
 
+            // imposto i Listener di click sulle CardView
             cvDiscount.setOnClickListener(this);
             cvComplimentaryServices.setOnClickListener(this);
             cvSpecialPackets.setOnClickListener(this);
         }
 
+        /**
+         * L'onClick, al click sulle CardView, gestisce l'apertura di un popup che mostra le offerte
+         * relative ad una categoria di sconti.
+         * @param v: la View che e' stata cliccata
+         */
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.cvDiscount:
-                    if (cntPopupWindow == 0) {
-                        showPopupWindow(R.id.cvDiscount);
-                        cntPopupWindow += 1;
+                    if (cntPopupWindow == 0) {  // verifica che non sia aperto alcun popup
+                        showPopupWindow(R.id.cvDiscount);   // creazione e visualizzazione del popup
+                        cntPopupWindow += 1;    // un popup e' stato aperto
                     }
                     break;
                 case R.id.cvComplimentaryServices:
-                    if (cntPopupWindow == 0) {
-                        showPopupWindow(R.id.cvComplimentaryServices);
-                        cntPopupWindow += 1;
+                    if (cntPopupWindow == 0) { // verifica che non sia aperto alcun popup
+                        showPopupWindow(R.id.cvComplimentaryServices);  // creazione e visualizzazione del popup
+                        cntPopupWindow += 1; // un popup e' stato aperto
                     }
                     break;
                 case R.id.cvSpecialPackets:
-                    if (cntPopupWindow == 0) {
-                        showPopupWindow(R.id.cvSpecialPackets);
-                        cntPopupWindow += 1;
+                    if (cntPopupWindow == 0) { // verifica che non sia aperto alcun popup
+                        showPopupWindow(R.id.cvSpecialPackets); // creazione e visualizzazione del popup
+                        cntPopupWindow += 1; // un popup e' stato aperto
                     }
                     break;
 
                 case R.id.ibtnClosePopup:
                     mPopupWindow.dismiss(); // Chiudo la finestra di popup
-                    cntPopupWindow = 0;
+                    cntPopupWindow = 0; // nessun popup aperto
             }
         }
 
+        /**
+         * showPopupWindow() gestisce la creazione e la visualizzazione della finestra di Popup
+         * @param id: e' l'id della CardView che e' stata cliccata e di cui bisogna aprire il popup
+         */
         public void showPopupWindow(int id) {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);  // Inizializzo una nuova istanza del LayoutInflater service
             View popup = null;
             if (id == R.id.cvDiscount) {
-                popup = inflater.inflate(R.layout.popup_discount, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
+                popup = inflater.inflate(R.layout.popup_discount, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popup
             }
             if (id == R.id.cvComplimentaryServices) {
-                popup = inflater.inflate(R.layout.popup_complimentary_services, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
+                popup = inflater.inflate(R.layout.popup_complimentary_services, null);    // Gonfio (inflate) il layout "popup_complimentary_services" all'interno della View popup
             }
             if (id == R.id.cvSpecialPackets) {
-                popup = inflater.inflate(R.layout.popup_special_packets, null);    // Gonfio (inflate) il layout "popup_discount" all'interno della View popupDiscount
+                popup = inflater.inflate(R.layout.popup_special_packets, null);    // Gonfio (inflate) il layout "popup_special_packets" all'interno della View popup
             }
 
             mPopupWindow = new PopupWindow(popup, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);   // Inizializzo una nuova istanza di una finestra popup
@@ -105,7 +119,7 @@ public class DealsActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-        finish();
+        startActivity(new Intent(getApplicationContext(), HomeActivity.class)); // torna alla HomeActivity
+        finish();   // termina la corrente Activity
     }
 }
